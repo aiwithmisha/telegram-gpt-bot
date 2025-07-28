a08e93be2e30902824ce1fc754a1fd0a0ae73e6f
aiwithmisha <legalconsulting30@gmail.com>
2025-07-28 16:49:44 +0300

    Update main.py
    Добавлен импорт pydub для работы с аудио

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import openai
+from pydub import AudioSegment
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
